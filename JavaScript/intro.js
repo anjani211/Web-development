e5bade9989aefc6706066bb6c3791cfafdf2efb2
Anjani Kumar Jha <79541509+anjani211@users.noreply.github.com>
2022-02-10 18:15:19 +0530

    Add files via upload

---
+++ JavaScript/intro.js
@@ -0,0 +1,13 @@
+//print
+console.log("hello ")
+//declare variable
+
+let a;
+a=10;
+console.log(a);
+
+a=10.1;
+a='i am good';
+a = 'i am also good'
+console.log(a)
+//variable types= primitive types: number, string, boolean, null
